049264f168f1670c286ac5cb46d8583a5225e539
Lucas <Lucaspdfe@gmail.com>
2025-07-08 13:30:47 -0300

    Removed Warning (Alpha 0.5)

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -1,7 +1,7 @@
 #include "stdint.h"
 #include "printf.h"
 
-void _cdecl cstart_(uint16_t bootDrive) {
+void _cdecl cstart_(/* uint16_t bootDrive */) {
     printf("Test \"%s\" '%c' <0x%x> %i u%u", "Test String", 'H', 16, -15, -15);
     for(;;);
 }
