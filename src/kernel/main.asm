c386a9fcfe214445d3ab7b1f650d2b2763f1f0b7
Lucas <Lucaspdfe@gmail.com>
2025-08-22 18:15:25 -0300

    Fixed error where i accidentaly put stage2 on kernel

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,23 +1,44 @@
+org 0x0
 bits 16
 
-section _ENTRY class=CODE
+start:
+    ; Clear the screen
+    mov ah, 0
+    mov al, 0x03
+    int 10h
 
-extern _cstart_
-global entry
-
-entry:
-    cli
-    ; setup stack
-    mov ax, ds
-    mov ss, ax
-    mov sp, 0
-    mov bp, sp
-    sti
-
-    ; expect boot drive in dl, send it as argument to cstart function
-    xor dh, dh
-    push dx
-    call _cstart_
-
+    ; Print "Hello, World from KERNEL!!"
+    mov si, msg
+    call puts
     cli
     hlt
+
+;
+; puts: prints a string to the screen
+; Parameters:
+;   ds:si: pointer to the string to print
+;
+puts:
+    push ax
+    push bx
+    push cx
+
+    mov ah, 0x0E
+    mov bh, 0x00
+    mov bl, 0x07
+
+    mov cx, 0xFFFF
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    loop .loop
+.done:
+    pop cx
+    pop bx
+    pop ax
+
+    ret
+
+msg: db 'Hello, World from KERNEL!!', 0
